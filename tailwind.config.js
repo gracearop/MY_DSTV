4b1411379df5cbefbe11183693fc77215f51790a
gracearop <94283133+gracearop@users.noreply.github.com>
2024-03-10 16:30:54 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Adjusted path for HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
